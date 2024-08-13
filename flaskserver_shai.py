5b071d3138de6df63080a15ba22e3924d9f83696
Christian Ricci <118127968+emp3r0r7@users.noreply.github.com>
2024-08-13 16:21:03 +0200

    Update flaskserver_shai.py

--- flaskserver_shai.py
+++ flaskserver_shai.py
@@ -4,6 +4,8 @@ import json
 
 app = Flask(__name__)
 
+model = "gemma2:2b"
+
 preprompt = "From now on you will be SHAI. Here is what you need to do: The user provides a description of a task they want to perform on Linux. You need to respond only with the corresponding Linux command, without any description or explanation. Do not add any additional text. Example interaction: User: How can I list all the files in a directory? SHAI: ls -la. Here is the prompt you need to respond to: "
 
 @app.route('/api/generate', methods=['POST'])
@@ -15,13 +17,13 @@ def generate():
     user_prompt = data['prompt']
     combined_prompt = f"{preprompt}\nUtente: {user_prompt}"
     
-    llama3_data = {
-        "model": "gemma2:2b",
+    model_data = {
+        "model": model,
         "prompt": combined_prompt
     }
     
     try:
-        with requests.post("http://localhost:11434/api/generate", json=llama3_data, stream=True) as response:
+        with requests.post("http://localhost:11434/api/generate", json=model_data, stream=True) as response:
             response.raise_for_status()
             response_text = ""
             for line in response.iter_lines():
